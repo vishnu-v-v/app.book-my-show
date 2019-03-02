f9896f6b050d8e1b27cabba8382349e2dfac9be8
Vishnu V V <ng.pingme@gmail.com>
2019-03-03 04:25:26 +0530

    Add axios and vue axios for AJAX calls

--- src/main.js
+++ src/main.js
@@ -2,11 +2,15 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import VueAxios from "vue-axios";
+import axios from "./api/axios";
 
 Vue.config.productionTip = false;
+Vue.use(VueAxios, axios);
 
 new Vue({
   router,
   store,
+  axios,
   render: h => h(App)
 }).$mount("#app");
