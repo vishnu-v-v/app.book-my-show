b1e96d535fafdb173bb812aee086754f426d1d40
Vishnu V V <ng.pingme@gmail.com>
2019-03-03 04:47:03 +0530

    Update the signin link

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/singin">Signin</router-link>
+      <router-link to="/signin">Signin</router-link>
     </div>
     <router-view />
   </div>
